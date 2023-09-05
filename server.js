5d8d11f18ee8d69d386a5fc27853780284286933
Sandy3122 <seeram87@gmail.com>
2023-09-05 18:15:56 +0530

    Added route in server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 8000; // Use port 3000 by default, or specify a
 // Serve static files from the "public" directory (where your HTML file is located)
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
