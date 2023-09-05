1d73e31d3b050a6c21e6bfdfc715360d8981e420
Sandy3122 <seeram87@gmail.com>
2023-09-05 18:19:04 +0530

    Small Changes has been made

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const port = process.env.PORT || 8000; // Use port 3000 by default, or specify a different port with an environment variable
 
-// Serve static files from the "public" directory (where your HTML file is located)
+const app = express();
+const port = process.env.PORT || 8000;
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
