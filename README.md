d8ff47c5c254459d66b48fc2455c89fc16edecd1
Sandy3122 <seeram87@gmail.com>
2023-09-05 17:58:11 +0530

    QR-Generator Has Completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# QR-Generator
+# QR Code Generator
+
+This is a simple web application that allows you to generate QR codes for URLs. Users can enter a URL, choose the QR code size, and generate a QR code image that can be downloaded.
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8000; // Use port 3000 by default, or specify a different port with an environment variable
+
+// Serve static files from the "public" directory (where your HTML file is located)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
